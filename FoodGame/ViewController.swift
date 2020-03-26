c5e039be18ee3665bf6ddc17e4a580ea33ce44f2
Lucas Claro <lucasbrclaro@gmail.com>
2020-03-26 17:19:21 -0300

    Initial Commit

---
+++ FoodGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodGame
+//
+//  Created by Lucas Claro on 26/03/20.
+//  Copyright © 2020 Lucas Claro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
