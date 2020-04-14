adf5d080d6cc8be44ee8e175a2393a1879b3d2fb
felipefhenriques <felipeferreirahenriques@outlook.com>
2020-04-14 16:38:21 -0300

    Switchs trocados por checkboxes e adicionadas mais duas condicoes

--- FoodGame/felipe.swift
+++ FoodGame/felipe.swift
@@ -161,94 +161,207 @@ class segunda: UIViewController, UITextFieldDelegate{
 
 
 class terceira: UIViewController{
-    @IBOutlet var temDiabetes: UISwitch!
-    @IBOutlet var temHipertensao: UISwitch!
-    @IBOutlet var temIntolerancia: UISwitch!
-    @IBOutlet var isVegetariano: UISwitch!
+    @IBOutlet var temDiab: UIButton!
+    @IBOutlet var temHipe: UIButton!
+    @IBOutlet var temInto: UIButton!
+    @IBOutlet var isVege: UIButton!
+    @IBOutlet var temGast: UIButton!
+    @IBOutlet var isGlut: UIButton!
+    
+    
     var vetor: [String] = []
     var i = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        temDiabetes.isOn = false
-        temHipertensao.isOn = false
-        temIntolerancia.isOn = false
-        isVegetariano.isOn = false
-        
         let userDefaults = UserDefaults.standard
         userDefaults.set(vetor, forKey: "vetor")
         
     }
     
-    @IBAction func switchDiab(_ sender: UISwitch) {
-        if temDiabetes.isOn{
+    @IBAction func checkDiabetes(_ sender: Any) {
+        if temDiab.isSelected {
+            temDiab.isSelected = false
+            print("off")
+            if let index = vetor.firstIndex(of: "1"){
+            vetor.remove(at: index)
+            print("Removido")
+            }
+        } else {
+            temDiab.isSelected = true
+            print("on")
             vetor.append("1")
             i += 1
         }
-        if temDiabetes.isOn == false{
-            if let index = vetor.firstIndex(of: "1"){
-                vetor.remove(at: index)
-                print("Removido")
-            }
-        }
     }
-    @IBAction func switchHiper(_ sender: UISwitch) {
-        if temHipertensao.isOn{
-            vetor.append("2")
-            i+=1
-        }
-        
-        if temHipertensao.isOn == false{
+    @IBAction func checkHiper(_ sender: UIButton) {
+        if temHipe.isSelected {
+           temHipe.isSelected = false
+            print("off")
             if let index = vetor.firstIndex(of: "2"){
-                vetor.remove(at: index)
-                print("Removido")
-            }
-        }
-        
-    }
-        
-    
-    @IBAction func switchInto(_ sender: UISwitch) {
-            if temIntolerancia.isOn {
-                vetor.append("3")
+            vetor.remove(at: index)
+            print("Removido")
+                    }
+            } else {
+                temHipe.isSelected = true
+                print("on")
+                vetor.append("2")
                 i+=1
-            }
-        
-            if temIntolerancia.isOn == false {
-                if let index = vetor.firstIndex(of: "3"){
-                    vetor.remove(at: index)
-                    print("Removido")
                 }
-            }
-    }
-    @IBAction func switchVeggie(_ sender: UISwitch) {
-        if isVegetariano.isOn{
-            vetor.append("4")
-            i+=1
-        }
-        
-        if isVegetariano.isOn == false{
-            if let index = vetor.firstIndex(of: "4"){
-                vetor.remove(at: index)
-                print("Removido")
-            }
-        }
     }
     
+    @IBAction func checkIntolerante(_ sender: UIButton) {
+        if temInto.isSelected {
+        temInto.isSelected = false
+         print("off")
+         if let index = vetor.firstIndex(of: "3"){
+         vetor.remove(at: index)
+         print("Removido")
+                 }
+         } else {
+             temInto.isSelected = true
+             print("on")
+             vetor.append("3")
+             i+=1
+             }
+    }
+    
+    @IBAction func checkVeggie(_ sender: UIButton) {
+        if isVege.isSelected {
+        isVege.isSelected = false
+         print("off")
+         if let index = vetor.firstIndex(of: "4"){
+         vetor.remove(at: index)
+         print("Removido")
+                 }
+         } else {
+             isVege.isSelected = true
+             print("on")
+             vetor.append("4")
+             i+=1
+             }
+    }
+    
+    @IBAction func checkGastrite(_ sender: UIButton) {
+        if temGast.isSelected {
+        temGast.isSelected = false
+         print("off")
+         if let index = vetor.firstIndex(of: "5"){
+         vetor.remove(at: index)
+         print("Removido")
+                 }
+         } else {
+             temGast.isSelected = true
+             print("on")
+             vetor.append("5")
+             i+=1
+             }
+    }
+    
+    @IBAction func checkGluten(_ sender: UIButton) {
+        if isGlut.isSelected {
+        isGlut.isSelected = false
+         print("off")
+         if let index = vetor.firstIndex(of: "6"){
+         vetor.remove(at: index)
+         print("Removido")
+                 }
+         } else {
+             isGlut.isSelected = true
+             print("on")
+             vetor.append("6")
+             i+=1
+             }
+    }
+    
+    
+//    @IBAction func checkHipertensao(_ sender: Any) {
+//        if temHipe.isSelected {
+//            temDiab.isSelected = false
+//            print("off")
+//            if let index = vetor.firstIndex(of: "2"){
+//            vetor.remove(at: index)
+//            print("Removido")
+//            }
+//        } else {
+//            temDiab.isSelected = true
+//            print("on")
+//            vetor.append("2")
+//            i+=1
+//        }
+//    }
+    
+
+    
+//    @IBAction func switchDiab(_ sender: UISwitch) {
+//        if temDiabetes.isOn{
+//            vetor.append("1")
+//            i += 1
+//        }
+//        if temDiabetes.isOn == false{
+//            if let index = vetor.firstIndex(of: "1"){
+//                vetor.remove(at: index)
+//                print("Removido")
+//            }
+//        }
+//    }
+//    @IBAction func switchHiper(_ sender: UISwitch) {
+//        if temHipertensao.isOn{
+//            vetor.append("2")
+//            i+=1
+//        }
+//
+//        if temHipertensao.isOn == false{
+//            if let index = vetor.firstIndex(of: "2"){
+//                vetor.remove(at: index)
+//                print("Removido")
+//            }
+//        }
+//
+//    }
+//
+//
+//    @IBAction func switchInto(_ sender: UISwitch) {
+//            if temIntolerancia.isOn {
+//                vetor.append("3")
+//                i+=1
+//            }
+//
+//            if temIntolerancia.isOn == false {
+//                if let index = vetor.firstIndex(of: "3"){
+//                    vetor.remove(at: index)
+//                    print("Removido")
+//                }
+//            }
+//    }
+//    @IBAction func switchVeggie(_ sender: UISwitch) {
+//        if isVegetariano.isOn{
+//            vetor.append("4")
+//            i+=1
+//        }
+//
+//        if isVegetariano.isOn == false{
+//            if let index = vetor.firstIndex(of: "4"){
+//                vetor.remove(at: index)
+//                print("Removido")
+//            }
+//        }
+//    }
+//
     @IBAction func imprime(_ sender: UIButton) {
         for i in 0..<vetor.count{
             print("vetor", vetor[i])
-        }
+       }
     }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "vetor"{
             if let prato = segue.destination as? prato{
                 prato.vetor = vetor
             }
         }
-    }
+   }
 }
 
 class dados{
